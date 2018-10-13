acb870d93bbdbf11d4f42899bddb93fec86e403b
Dan Andersson <760686+daon@users.noreply.github.com>
2018-10-13 13:46:10 +0200

    add solution

--- src/game.test.ts
+++ src/game.test.ts
@@ -6,12 +6,51 @@ describe("Bowling Game", () => {
     game = new Game();
   });
 
-  test('all rolls in the gutter', () => {
-    const rolls = new Array(20);
-    rolls.forEach(() => {
-        game.roll(0);
+  const manyRolls = (rolls: number, pins: number) => {
+    new Array<number>(rolls).fill(pins).forEach(pins => {
+      game.roll(pins);
     });
+  };
 
+  const rollSpare = () => {
+    game.roll(5);
+    game.roll(5);
+  };
+
+  const rollStrike = () => {
+    game.roll(10);
+  };
+
+  test("all rolls in the gutter", () => {
+    manyRolls(20, 0);
     expect(game.getScore()).toBe(0);
+  });
+
+  test("all rolls hit one pin", () => {
+    manyRolls(20, 1);
+
+    expect(game.getScore()).toBe(20);
+  });
+
+  test("rolls one spare", () => {
+    rollSpare();
+    game.roll(3);
+    manyRolls(17, 0);
+
+    expect(game.getScore()).toBe(16);
+  });
+
+  test("rolls one srike", () => {
+    rollStrike();
+    game.roll(4);
+    game.roll(3);
+    manyRolls(16, 0);
+
+    expect(game.getScore()).toBe(24);
+  });
+
+  test("rolls perfect game", () => {
+    manyRolls(20, 10);
+    expect(game.getScore()).toBe(300);
   });
 });
--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,46 @@
 export class Game {
+  private rolls: number[] = new Array(21).fill(0);
+  private currentRoll = 0;
+
   roll(pins: number) {
-    return pins;
+    this.rolls[this.currentRoll++] = pins;
   }
+
   getScore() {
-    return 0;
+    let score = 0;
+    let frameIndex = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.isStrike(frameIndex)) {
+        score += 10 + this.strikeBonus(frameIndex);
+        frameIndex++;
+      } else if (this.isSpare(frameIndex)) {
+        score += 10 + this.spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += this.sumOfPinsInFrame(frameIndex);
+        frameIndex += 2;
+      }
+    }
+    return score;
+  }
+
+  private sumOfPinsInFrame(frameIndex: number) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+  }
+
+  private strikeBonus(frameIndex: number) {
+    return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+  }
+
+  private spareBonus(frameIndex: number) {
+    return this.rolls[frameIndex + 2];
+  }
+
+  private isStrike(frameIndex: number) {
+    return this.rolls[frameIndex] === 10;
+  }
+
+  private isSpare(frameIndex: number) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
   }
 }
